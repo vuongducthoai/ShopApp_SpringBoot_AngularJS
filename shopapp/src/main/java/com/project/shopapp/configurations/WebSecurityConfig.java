7e75ad376ce50aead9b2367aa4c7e7a4a1814ff0
vuongducthoai <thoai12309@gmail.com>
2025-03-23 15:04:15 +0700

    commit

--- shopapp/src/main/java/com/project/shopapp/configurations/WebSecurityConfig.java
+++ shopapp/src/main/java/com/project/shopapp/configurations/WebSecurityConfig.java
@@ -1,12 +1,26 @@
 package com.project.shopapp.configurations;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class WebSecurityConfig {
-    
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(request -> {
+                    request.requestMatchers("**").permitAll();
+                    
+                });
+        return http.build();
+    }
+
 }
